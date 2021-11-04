29fa3739349a6b211bf0de9a7e8197db9440fd41
Claire Davies <claire.davies@junitech.com>
2021-11-04 15:25:58 +0000

    creating template for creature list, and linking it to app module

---
+++ fantasy-creatures/src/app/Creature/creature-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "pm-creatures",
+    templateUrl: "./creature-list.component.html",
+    styleUrls: ["./creature-list.component.css"]
+})
+
+export class CreatureListComponent {
+    pageTitle: string = "Creatures";
+
+}
